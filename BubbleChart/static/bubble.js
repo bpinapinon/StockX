af3f134800b954a54000f42172972c232eeff58d
Madeline <mseif15@berkeley.edu>
2018-10-16 19:57:08 -0700

    progress

---
+++ BubbleChart/static/bubble.js
@@ -0,0 +1,40 @@
+var url = "/bubbledata";
+
+
+function buildPlot() {
+    d3.json(url).then(function(error, response) {
+
+        console.log(response);
+
+        var trace = {
+            mode: "markers",
+            type: "scatter",
+            x: response.map(sneakers => sneakers.retailPrice),
+            y: response.map(sneakers => sneakers.avgSalePrice), 
+            hovertext: response.map(sneakers => sneakers.brand),
+            marker: {
+            size: response.map(sneakers => sneakers.noSales),
+            color: response.map(sneakers => sneakers.color),
+            sizemode:'area'
+
+            }
+        };
+
+        var data = [trace];
+
+        var layout = {
+            title: "Bubble Plot",
+            xaxis: {title: "Retail Price ($)"},
+            yaxis: {title: "Average Sale Price ($)"},
+            hovermode: "closest"
+        };
+
+    Plotly.plot("bubble", data, layout);
+    });
+}
+buildPlot();
+
+
+
+
+
